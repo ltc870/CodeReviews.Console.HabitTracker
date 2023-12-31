64e7814b540546c0bdd258f70c77758e83e41d09
Lawrence C <lawrencecaudle870@gmail.com>
2023-12-30 23:49:26 -0500

    started habit logger app

---
+++ HabitLogger.ltc870/Program.cs
@@ -0,0 +1,48 @@
+﻿using Spectre.Console;
+
+//CreateDatabase();
+
+MainMenu();
+
+void MainMenu()
+{
+    var isMenuRunning = true;
+
+    while (isMenuRunning)
+    {
+        var usersChoice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("What would you like to do?")
+            .AddChoices(
+                "Add Record",
+                "Delete Record",
+                "View Records",
+                "Update Record",
+                "Quit")
+            );
+
+        switch (usersChoice)
+        {
+            case "Add Record":
+                //AddRecord();
+                break;
+            case "Delete Record":
+                //DeleteRecord();
+                break;
+            case "View Records":
+                //ViewRecords();
+                break;
+            case "Update Record":
+                //UpdateRecord();
+                break;
+            case "Quit":
+                Console.WriteLine("Goodbye!");
+                isMenuRunning = false;
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please choose one from above.");
+                break;
+        }
+    }
+}
+
